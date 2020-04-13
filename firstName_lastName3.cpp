4bf1c9d6977094b27f76fa0be8ce89c05ba72347
Niksol15 <44037157+Niksol15@users.noreply.github.com>
2020-04-13 22:36:52 +0300

    Update and rename main.cpp to firstName_lastName3.cpp

--- firstName_lastName3.cpp
+++ firstName_lastName3.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <map>
 #include <deque>
-#include <iostream>
 using namespace std;
 
 class Person {
@@ -159,19 +158,3 @@ private:
     map<int, string> firstName;
     int birthday;
 };
-/*
-int main() {
-    Person person("Polina", "Sergeeva", 1960);
-    for (int year : {1959, 1960}) {
-        cout << person.GetFullNameWithHistory(year) << endl;
-    }
-
-    person.ChangeFirstName(1965, "Appolinaria");
-    person.ChangeLastName(1967, "Ivanova");
-    for (int year : {1965, 1967}) {
-        cout << person.GetFullNameWithHistory(year) << endl;
-    }
-
-    return 0;
-}
- */
